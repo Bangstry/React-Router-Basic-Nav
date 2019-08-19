41a83655264f709a4b1ed90038295001ad4af49b
Bangstry <tylerwesleyquinn@gmail.com>
2019-08-19 15:49:11 -0400

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+const About = (props) => {
+    console.log('About', props);
+    console.log(props.match.params.id);
+    return (
+        <h1>
+          About {props.match.params.id}
+          <Route path ={`${props.match.path}/tyler`} render={props => <div>tyler</div>}/>
+          <Route path ={`${props.match.path}/zach`} render={props => <div>zach</div>}/>
+          <button onClick={() => {
+            console.log('We CLicked the Button!');
+            props.history.push('/');
+        }}> Navigate to Home</button>
+        <button onClick={() => {
+            console.log('We CLicked Back!');
+            props.history.goBack();
+        }}> Go Back</button>
+        </h1>
+    );
+};
+    
 
-const About = () => <h1>About</h1>;
 
 export default About;
